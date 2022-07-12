c2b976bc4f11d49ec294d614f706cabb0ffcde7c
Natan Smutek <veat.xd@gmail.com>
2022-07-12 10:39:25 +0200

    implementation of homepage

---
+++ data/data.php
@@ -0,0 +1,40 @@
+
+        <?php
+
+require_once './partials/header.php';
+
+$images=[
+[
+    "title"=>"Deep Earth",
+    "link"=>"images/desktop/image-curiosity.jpg"
+],
+[
+    "title"=>"Night Arcade",
+    "link"=>"images/desktop/image-deep-earth.jpg"
+],
+[
+    "title"=>"Deep Earth",
+    "link"=>"images/desktop/image-fisheye.jpg"
+],
+[
+    "title"=>"Night Arcade",
+    "link"=>"images/desktop/image-grid.jpg"
+],
+[
+    "title"=>"Deep Earth",
+    "link"=>"images/desktop/image-curiosity.jpg"
+],
+[
+    "title"=>"Night Arcade",
+    "link"=>"images/desktop/image-deep-earth.jpg"
+],
+[
+    "title" => "Deep Earth",
+    "link" => "images/desktop/image-fisheye.jpg"
+  ],
+  [
+      "title" => "Night Arcade",
+      "link" => "images/desktop/image-grid.jpg"
+  ]
+
+  ];
---
+++ partials/footer.php
@@ -0,0 +1,34 @@
+<footer class="pageFooter">
+     
+<div class="innerWrapper">
+    
+
+     
+     <div class="footerTop">
+     <a href="">
+     <img src="images/logo.svg" alt="">
+     </a>     
+     <div>
+     <a href=""><img src="images/icon-facebook.svg" alt=""></a>
+<a href=""><img src="images/icon-instagram.svg" alt=""></a>
+<a href=""><img src="images/icon-twitter.svg" alt=""></a>
+<a href=""><img src="images/icon-pinterest.svg" alt=""></a>
+
+     </div>
+
+
+</div>
+    
+    <div class="footerBottom">
+    <?php require "partials/menu.php" ?>
+    <span>@ 2022 Loopstudios. All rights reserved</span>
+    </div>
+    
+     </div>
+
+
+     <p>Designed by XYZ 2022</p>
+</footer>
+
+</body>
+</html>
